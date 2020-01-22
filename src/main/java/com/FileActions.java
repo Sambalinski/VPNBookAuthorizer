cb4926ed7e7cd1fc5ec6236653af01b005ebdf51
Johnny Sambalinski <antelavaen@gmail.com>
2020-01-22 11:35:55 +0300

    added javadoc

--- src/main/java/com/FileActions.java
+++ src/main/java/com/FileActions.java
@@ -11,15 +11,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileActions {
-
-    //Getting the parent directory of the executable jar file
-    //Получение родительского каталога исполняемого jar-файла
+    
+    /**
+     * Get the parent directory of the executable jar file
+     * Получить родительский каталог исполняемого jar-файла
+     *
+     * @return - path
+     * @throws URISyntaxException
+     */
     static Path getCurrentDirectory() throws URISyntaxException {
         return Paths.get(FileActions.class.getProtectionDomain().getCodeSource().getLocation().toURI());
     }
-
-    //Read the contents of the file
-    //Считываем содержимое файла
+    
+    
+    /**
+     * Read the contents of the file
+     * Считать содержимое файла
+     *
+     * @param path
+     * @return
+     */
     static List<String> readAllLines(Path path) {
         try {
             return Files.readAllLines(path);
@@ -28,10 +39,16 @@ public class FileActions {
         }
         return null;
     }
-
-
-    //Replace line with  username & password
-    //Заменяем строку без указания на строку с указанием на файл с логином и паролем
+    
+    
+    /**
+     * Replace line with  username & password
+     * Заменить строку без указания на строку с указанием на файл с логином и паролем
+     *
+     * @param list
+     * @param auth
+     * @return
+     */
     static boolean replacer(ArrayList<String> list, String auth) {
         boolean changed = false;
         for (int i = 0; i < list.size(); i++) {
@@ -39,14 +56,20 @@ public class FileActions {
             if (s.equals("auth-user-pass")) {
                 changed = true;
                 String replacement = s.concat(" ").concat(auth);
-                list.add(i, replacement);
+                list.set(i, replacement);
             }
         }
         return changed;
     }
-
-    //Write the changed list to a file
-    //Запись изменённого списка в файл
+    
+    
+    /**
+     * Write the changed list to a file
+     * Запись изменённого списка в файл
+     *
+     * @param list
+     * @param fileName
+     */
     static void write(List<String> list, String fileName) {
         File file = new File(fileName);
         file.setWritable(true);
